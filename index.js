43ea0f022975ac181c9ae41e939e002cb050f3ac
Lisek282 <114082373+Lisek282@users.noreply.github.com>
2023-02-04 15:21:25 +0100

    Change playRound function

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ function palayRound(playerSelection = 'rock'){
   // compare player choice with computer choice
   switch(playerSelection) {
     case 'rock':
-      return computerSelection === 'rock' ? `It's a tie ${playerSelection} do not beats ${computerSelection}` : computerSelection === 'paper' ? `You Lose! ${computerSelection} beats ${playerSelection}` : `You Win! ${playerSelection} beats ${computerSelection}`
+      return computerSelection === 'rock' ? `tie` : computerSelection === 'paper' ? `lose` : `win`
     case 'paper':
-      return computerSelection === 'paper' ? `It's a tie ${playerSelection} do not beats ${computerSelection}` : computerSelection === 'scissors' ? `You Lose! ${computerSelection} beats ${playerSelection}` : `You Win! ${playerSelection} beats ${computerSelection}`
+      return computerSelection === 'paper' ? `tie` : computerSelection === 'scissors' ? `lose` : `win`
     case 'scissors':
-      return computerSelection === 'scissors' ? `It's a tie ${playerSelection} do not beats ${computerSelection}` : computerSelection === 'rock' ? `You Lose! ${computerSelection} beats ${playerSelection}` : `You Win! ${playerSelection} beats ${computerSelection}`
+      return computerSelection === 'scissors' ? `tie` : computerSelection === 'rock' ? `lose` : `win`
     default:
       return "Choice correct value: Rock, Paper or Scissors"
     
@@ -27,7 +27,7 @@ function palayRound(playerSelection = 'rock'){
 }
 // play the game 5 times
 function game(){
-  
+
 }
 
 
